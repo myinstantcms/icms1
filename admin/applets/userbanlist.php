myinstantcms/icms1

commit 91e3e8a847fb12ae8a42cdb395747fb5d23d2599

PHP 8.2 compatibility: replace create_function() and get_magic_quotes_gpc()
- Replace deprecated create_function() with closures in:
  - function.city_input.php (Smarty plugin)
  - bbcode.lib.php
  - usergroups.php applet
  - userbanlist.php applet
- Remove get_magic_quotes_gpc() check (always returns false since PHP 5.4)
- Add #[AllowDynamicProperties] to cmsUser class
- Update readme.php to indicate PHP 8.2+ support

admin/applets/userbanlist.php
@@ -76,7 +76,7 @@ function applet_userbanlist(){
                        'autodelete'=>array('autodelete', 'int', 0),
                        'int_num'=>array('int_num', 'int', 0),
                        'int_period'=>array('int_period', 'str', '',
-                                        create_function('$p', 'if(!in_array($p, array("MONTH","DAY","HOUR","MINUTE"))){ $p = "MINUTE"; } return $p;')));
+                                         function($p) { if(!in_array($p, array("MONTH","DAY","HOUR","MINUTE"))){ $p = "MINUTE"; } return $p; })));
 
         $items = cmsCore::getArrayFromRequest($types);
 

admin/applets/usergroups.php
@@ -77,7 +77,7 @@ function applet_usergroups(){
                        'alias'=>array('alias', 'str', ''),
                        'is_admin'=>array('is_admin', 'int', 0),
                        'access'=>array('access', 'array_str', array(),
-                                        create_function('$a_list', 'return implode(\',\', $a_list);')));
+                                         function($a_list) { return implode(',', $a_list); }));
 
         $items = cmsCore::getArrayFromRequest($types);
 

core/classes/user.class.php
@@ -14,6 +14,7 @@ define('USER_UPDATER', -1);
 define('USER_MASSMAIL', -2);
 define('ONLINE_INTERVAL', 3); // интервал в минутах, когда пользователь считается online
 
+#[AllowDynamicProperties]
 class cmsUser {
 
     const PROFILE_LINK_PREFIX  = 'users/';

core/cms.php
@@ -2145,8 +2145,7 @@ class cmsCore {
         }
 
         $string = trim((string)$input);
-        //Если magic_quotes_gpc = On, сначала убираем экранирование
-        $string = (@get_magic_quotes_gpc()) ? stripslashes($string) : $string;
+        $string = stripslashes($string);
         $string = rtrim($string, ' \\');
         if ($strip_tags) {
             $string = cmsDatabase::getInstance()->escape_string(strip_tags($string));

includes/bbcode/bbcode.lib.php
@@ -919,10 +919,13 @@ class bbcode {
 
         return preg_replace_callback(
           '#<a href="/go/url=([^"]+)"#',
-          create_function(
-              '$matches',
-              'if (!strstr($matches[1], $_SERVER[\'HTTP_HOST\'])){ return "<a rel=\"nofollow\" title=\"".htmlspecialchars($matches[1])."\" href=\"/go/url=-".base64_encode($matches[1])."\""; } else { return "<a href=\"".htmlspecialchars($matches[1])."\""; }'
-          ),
+          function($matches) {
+              if (!strstr($matches[1], $_SERVER['HTTP_HOST'])) {
+                  return '<a rel="nofollow" title="'.htmlspecialchars($matches[1]).'" href="/go/url=-'.base64_encode($matches[1]).'"';
+              } else {
+                  return '<a href="'.htmlspecialchars($matches[1]).'"';
+              }
+          },
           $text
         );
 

includes/smarty/libs/plugins/function.city_input.php
@@ -18,7 +18,7 @@
             return '<input type="text" value="'.htmlspecialchars($params['value']).'" name="'.htmlspecialchars($params['name']).'" class="text-input" style="width:300px"/>';
         }
 
-        array_walk($params, create_function('&$value', 'return htmlspecialchars($value);'));
+        array_walk($params, function(&$value) { $value = htmlspecialchars($value); });
 
         if (!isset($params['placeholder'])) {
             $params['placeholder'] = $_LANG['SELECT_CITY'];

commit bf28be5e4c54b4fda517ba4467e63ced8e748d9c

Fix fatal syntax error in admin userbanlist applet
Regression from commit 91e3e8a (create_function to closure conversion):
an extra closing paren made the file unparseable on any PHP version.
The user ban list admin applet is working again.

admin/applets/userbanlist.php
@@ -76,7 +76,7 @@ function applet_userbanlist(){
                        'autodelete'=>array('autodelete', 'int', 0),
                        'int_num'=>array('int_num', 'int', 0),
                        'int_period'=>array('int_period', 'str', '',
-                                         function($p) { if(!in_array($p, array("MONTH","DAY","HOUR","MINUTE"))){ $p = "MINUTE"; } return $p; })));
+                                         function($p) { if(!in_array($p, array("MONTH","DAY","HOUR","MINUTE"))){ $p = "MINUTE"; } return $p; }));
 
         $items = cmsCore::getArrayFromRequest($types);
 