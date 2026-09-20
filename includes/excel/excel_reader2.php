dfacad11431320351e3c0819e35cba330b349dd0
maxisoft <support@maxicms.ru>
2026-09-20 15:20:47 +0300

    excel_reader2: fix implicitly required parameter deprecation
    Optional $sheet declared before required $prop (deprecated since 8.0);
    all call sites pass both arguments, defaulting $prop to null is safe.

--- includes/excel/excel_reader2.php
+++ includes/excel/excel_reader2.php
@@ -533,7 +533,7 @@ class Spreadsheet_Excel_Reader {
 		}
 		return null;
 	}
-	function fontProperty($row,$col,$sheet=0,$prop) {
+	function fontProperty($row,$col,$sheet=0,$prop=null) {
 		$font = $this->fontRecord($row,$col,$sheet);
 		if ($font!=null) {
 			return $font[$prop];
